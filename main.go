abdcd24899e57017b58eb7ff0a3ebf304b28cf62
mishaRomanov <mishanyaromanov910@gmail.com>
2023-09-06 16:59:08 +0300

    update main.go

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ import (
 var requestID int //this variable is used to keep track of each request
 // in case something goes bad we can track it in our server logs
 
-//TasksTracker is a storage for our tasks
-var TasksTracker = map[int]Task{}
+// TasksTracker is a storage for our tasks
+var TasksTracker = map[int]*Task{}
 
 type Task struct {
 	Id   int    `json:"id"`
@@ -21,6 +21,12 @@ type Task struct {
 	Time time.Time
 }
 
+// UpdateTask is used to unmrashall all jsons sent to PATCH tasks
+type UpdateTask struct {
+	Desc string `json:"desc"`
+}
+
+// About func returns information about our app
 func About(c *fiber.Ctx) error {
 	requestID++
 	logrus.Infof("New About request number %d at:%s", requestID, time.Now().Format(time.RFC822))
@@ -45,7 +51,7 @@ func main() {
 		id, err := strconv.Atoi(param)
 		if err != nil {
 			logrus.Infof("Error while converting string to int in request number %d", requestID)
-			return ctx.Status(fiber.StatusConflict).SendString(fmt.Sprintf("Error:%w", err))
+			return ctx.Status(fiber.StatusConflict).SendString(fmt.Sprintf("Error:%v", err))
 		}
 		logrus.Infof("Succesfull request number %d", requestID)
 		return ctx.Status(fiber.StatusOK).JSON(TasksTracker[id])
@@ -62,9 +68,30 @@ func main() {
 			return ctx.Status(fiber.StatusUnprocessableEntity).SendString("Error while creating a new task!")
 		}
 		//adding a new task
-		TasksTracker[newTask.Id] = newTask
+		TasksTracker[newTask.Id] = &newTask
 		logrus.Infof("New task added: request number %d", requestID)
 		return ctx.Status(fiber.StatusOK).SendString(fmt.Sprintf("A new task created! Unique task id is:%d", newTask.Id))
+	})
+
+	//PATCH already existing task
+	webApp.Patch("/tasks/:id", func(ctx *fiber.Ctx) error {
+		requestID++
+		param := ctx.Params("id")
+		id, err := strconv.Atoi(param)
+		if err != nil {
+			logrus.Infof("Error while converting string to int in request number %d", requestID)
+		}
+		new := UpdateTask{}
+		err = ctx.BodyParser(&new)
+		if err != nil {
+			logrus.Infof("Error while unmarshalling json to struct at request number %d: %v", requestID, err)
+		}
+		old := TasksTracker[id].Desc
+
+		TasksTracker[id].Desc = new.Desc
+		logrus.Infof("Updated task number %d with request %d", id, requestID)
+		return ctx.Status(fiber.StatusOK).SendString(fmt.Sprintf(`Updated your task from "%s" to "%s"`, old, TasksTracker[id].Desc))
+
 	})
 
 	logrus.Fatal(webApp.Listen(":8080"))
