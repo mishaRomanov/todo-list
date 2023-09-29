4f0b04bb9fb0b19e8fc5d97534e55b998bb60354
mishaRomanov <mishanyaromanov910@gmail.com>
2023-09-29 20:48:29 +0300

    update main.go

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.20
 require (
 	fyne.io/fyne/v2 v2.3.5
 	github.com/gofiber/fiber v1.14.6
-	github.com/gofiber/fiber/v2 v2.47.0
+	github.com/gofiber/fiber/v2 v2.49.2
 	github.com/sirupsen/logrus v1.9.3
 )
 
@@ -28,15 +28,15 @@ require (
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/gofiber/utils v0.0.10 // indirect
 	github.com/goki/freetype v0.0.0-20220119013949-7a161fd3728c // indirect
-	github.com/google/uuid v1.3.0 // indirect
+	github.com/google/uuid v1.3.1 // indirect
 	github.com/gopherjs/gopherjs v1.17.2 // indirect
 	github.com/gorilla/schema v1.1.0 // indirect
 	github.com/jsummers/gobmp v0.0.0-20151104160322-e2ba15ffa76e // indirect
-	github.com/klauspost/compress v1.16.3 // indirect
+	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
-	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/philhofer/fwd v1.1.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
@@ -48,14 +48,14 @@ require (
 	github.com/tevino/abool v1.2.0 // indirect
 	github.com/tinylib/msgp v1.1.8 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.47.0 // indirect
+	github.com/valyala/fasthttp v1.49.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	github.com/yuin/goldmark v1.4.13 // indirect
 	golang.org/x/crypto v0.7.0 // indirect
 	golang.org/x/image v0.3.0 // indirect
 	golang.org/x/mobile v0.0.0-20211207041440-4e6c2922fdee // indirect
 	golang.org/x/net v0.8.0 // indirect
-	golang.org/x/sys v0.9.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	honnef.co/go/js/dom v0.0.0-20210725211120-f030747120f2 // indirect
--- main.go
+++ main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"fmt"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
+	"os"
 	"strconv"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/sirupsen/logrus"
 )
-
-var requestID int //this variable is used to keep track of each request
-// in case something goes bad we can track it in our server logs
 
 // TasksTracker is a storage for our tasks
 var TasksTracker = map[int]*Task{}
@@ -27,16 +27,26 @@ type UpdateTask struct {
 
 // About func returns information about our app
 func About(c *fiber.Ctx) error {
-	requestID++
-	logrus.Infof("New About request number %d at:%s", requestID, time.Now().Format(time.RFC822))
+	logrus.Infof("New About request  at:%s", time.Now().Format(time.RFC822))
 	return c.SendString(`This small app is CRUD to-do list-type application.
 Send a POST-request to create a new task: /tasks/new 
 and monitor it by visiting /tasks`)
 }
 
 func main() {
+	os.Create("logs.log")
+	file, err := os.OpenFile("logs.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		logrus.Fatalf("%v", err)
+	}
 	// start a server
 	webApp := fiber.New()
+
+	webApp.Use(requestid.New())
+	webApp.Use(logger.New(logger.Config{
+		Format: "[${locals:requestid}]: ${method} ${path} - ${status}\n",
+		Output: file,
+	}))
 
 	//all handlers are stored here
 	webApp.Get("/about", About)
@@ -53,60 +63,56 @@ func main() {
 
 	//func that returns specific task
 	webApp.Get("/tasks/:id", func(ctx *fiber.Ctx) error {
-		requestID++
 		param := ctx.Params("id")
 		id, err := strconv.Atoi(param)
 		if err != nil {
-			logrus.Infof("Error while converting string to int in request number %d", requestID)
+			logrus.Infof("Error while converting string to int")
 			return ctx.Status(fiber.StatusConflict).SendString(fmt.Sprintf(`Invalid id "%s"`, param))
 		}
-		logrus.Infof("Succesfull request number %d", requestID)
+		logrus.Infof("Succesfull request")
 		return ctx.Status(fiber.StatusOK).JSON(TasksTracker[id])
 
 	})
 
 	//POSTS a new task
 	webApp.Post("/tasks/new", func(ctx *fiber.Ctx) error {
-		requestID++
 		newTask := Task{}
 		err := ctx.BodyParser(&newTask)
 		if err != nil {
-			logrus.Infof("Error while unmarshalling json to struct at request number %d: %v", requestID, err)
+			logrus.Infof("Error while unmarshalling json to struct at request %v", err)
 			return ctx.Status(fiber.StatusUnprocessableEntity).SendString("Error while creating a new task!")
 		}
 		//adding a new task
 		TasksTracker[newTask.Id] = &newTask
-		logrus.Infof("New task added: request number %d", requestID)
+		logrus.Infof("New task added")
 		return ctx.Status(fiber.StatusOK).SendString(fmt.Sprintf("A new task created! Unique task id is:%d", newTask.Id))
 	})
 
 	//PATCH already existing task
 	webApp.Patch("/tasks/:id", func(ctx *fiber.Ctx) error {
-		requestID++
 		param := ctx.Params("id")
 		id, err := strconv.Atoi(param)
 		if err != nil {
-			logrus.Infof("Error while converting string to int in request number %d", requestID)
+			logrus.Infof("Error while converting string to int")
 		}
 		new := UpdateTask{}
 		err = ctx.BodyParser(&new)
 		if err != nil {
-			logrus.Infof("Error while unmarshalling json to struct at request number %d: %v", requestID, err)
+			logrus.Infof("Error while unmarshalling json to struct %v", err)
 		}
 		old := TasksTracker[id].Desc
 
 		TasksTracker[id].Desc = new.Desc
-		logrus.Infof("Updated task number %d with request %d", id, requestID)
+		logrus.Infof("Updated task number %d", id)
 		return ctx.Status(fiber.StatusOK).SendString(fmt.Sprintf(`Updated your task from "%s" to "%s"`, old, TasksTracker[id].Desc))
 
 	})
 	//DELETE a task by id
 	webApp.Delete("/tasks/:id", func(ctx *fiber.Ctx) error {
-		requestID++
 		param := ctx.Params("id")
 		id, err := strconv.Atoi(param)
 		if err != nil {
-			logrus.Infof("Error while converting string to int in request number %d", requestID)
+			logrus.Infof("Error while converting string to int")
 		}
 		delete(TasksTracker, id)
 		logrus.Infof("Deleted task number %d", id)
