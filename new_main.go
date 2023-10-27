470dde94b8e2f8869e4e0f2ee5c2a2762338087e
mishaRomanov <mishanyaromanov910@gmail.com>
2023-10-27 20:48:30 +0300

    update new_main.go

--- new_main.go
+++ new_main.go
@@ -1,27 +1,40 @@
 package main
 
 import (
+	"encoding/json"
+	database "github.com/mishaRomanov/learn-fiber/db"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
-var TasksTracker = map[int]*Task{}
-
+// UpdateTask is used to unmrashall all jsons sent to PATCH tasks
 type Task struct {
 	Id   int    `json:"id"`
 	Desc string `json:"desc"`
 }
 
-// UpdateTask is used to unmrashall all jsons sent to PATCH tasks
-type UpdateTask struct {
-	Desc string `json:"desc"`
-}
-
 func main() {
+
+	log.Println(`|------------Starting a server!------------|`)
+	defer func() {
+		if r := recover(); r != nil {
+			log.Printf("Restored after a panic! The problem was: %v", r)
+		}
+	}()
+
+	//create a server
+	server := http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  300 * time.Millisecond,
+		WriteTimeout: 300 * time.Millisecond,
+	}
+	db := database.OpenDb()
 	//create a new logger
 	var logger log.Logger
-	file, err := os.Open("logs")
+	file, err := os.OpenFile("logs", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Print("Error while opening a file: %v", err)
 	}
@@ -29,9 +42,54 @@ func main() {
 	defer file.Close()
 
 	//we define the output of our logger
+	//this is very simple
 	logger.SetOutput(file)
 
+	//Handles /about request
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		logger.Println("New", r.Method, "request")
+		switch r.Method {
+		case http.MethodGet:
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(`This small app is CRUD to-do list-type application.
+Send a POST-request to create a new task: /tasks/new 
+and monitor it by visiting /tasks`))
+		default:
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Method not allowed"))
+		}
+	})
+
+	//handler that adds a new task into a database
+	http.HandleFunc("/tasks/add", func(w http.ResponseWriter, r *http.Request) {
+		newTask := &Task{}
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			logger.Printf("Error while reading the request body! %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Internal server error"))
+			return
+		}
+		err = json.Unmarshal(body, newTask)
+		if err != nil {
+			logger.Printf("Error while parsing the request body! %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Internal server error"))
+			return
+		}
+		result, err := db.Exec("INSERT INTO tasks (id,description) VALUES", newTask.Id, newTask.Desc)
+		if err != nil {
+			logger.Printf("Error while inserting values into database! %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Internal server error"))
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Data written. New task added"))
+		log.Println(result.RowsAffected())
+	})
+
 	//here we start a server
-	logger.Fatal(http.ListenAndServe(":8080", nil))
+	logger.Fatal(server.ListenAndServe())
 
 }
